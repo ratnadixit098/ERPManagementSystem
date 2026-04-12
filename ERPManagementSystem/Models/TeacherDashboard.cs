32ae9a2dfb0d41fb5ca29d78c3c74e8c697fafc2
ratnadixist <ratnadixist@gmail.com>
2026-04-12 14:45:40 +0530

    Signed-off-by: ratnadixist <ratnadixist@gmail.com>

---
+++ ERPManagementSystem/Models/TeacherDashboard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ERPManagementSystem.Models
+{
+    public class TeacherDashboard
+    {
+        public string DayOfWeek { get; set; }
+        public string Slot { get; set; }
+        public string SubjectName { get; set; }
+        public string ClassName { get; set; }
+        public bool IsAttendanceTaken { get; set; }
+    }
+}
